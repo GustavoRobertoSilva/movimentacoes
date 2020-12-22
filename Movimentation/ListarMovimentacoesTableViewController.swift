2642421e3fea254de1a5f3b6a9e368dde68d1368
Gustavo Roberto Silva <gustaoroberto@gmail.com>
2020-12-22 02:17:47 -0800

    Primeira Versao
    Primeira Versao

---
+++ Movimentation/ListarMovimentacoesTableViewController.swift
@@ -0,0 +1,158 @@
+//
+//  ListarMovimentacoesTableViewController.swift
+//  Movimentation
+//
+//  Created by Fabio Pizanelli Zinato on 12/18/20.
+//  Copyright © 2020 home. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class ListarMovimentacoesTableViewController: UITableViewController {
+
+    var context:NSManagedObjectContext!
+    var movimentacoes: [NSManagedObject] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        context = appDelegate.persistentContainer.viewContext
+     
+
+
+    }
+
+    func recuperarMovimentacoes(){
+        
+        let requisicao = NSFetchRequest<NSFetchRequestResult>(entityName: "Movement")
+        
+        let ordenacao = NSSortDescriptor.init(key: "data", ascending: false)
+        
+        requisicao.sortDescriptors = [ordenacao]
+        
+        do {
+            let movimentacoesRecuperadas = try context.fetch(requisicao)
+            
+            self.movimentacoes = movimentacoesRecuperadas as! [NSManagedObject]
+            
+            self.tableView.reloadData()
+            
+            print("Recuperou \(movimentacoes.count) Movimentacoes")
+            
+        } catch let erro {
+            print("Erro ao recuperar Movimentacoes : \(erro.localizedDescription) ")
+        }
+    }
+    
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return self.movimentacoes.count
+    }
+
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "celula", for: indexPath)
+        
+        let movimentacao = self.movimentacoes[indexPath.row]
+        
+        let descricaoRecuperado = movimentacao.value(forKey: "descricao")
+        let dataRecuperada = movimentacao.value(forKey: "data")
+        let tipoRecuperado = movimentacao.value(forKey: "tipo")
+        let valorRecuperado = movimentacao.value(forKey: "valor")
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd/mm/yyyy hh:mm"
+        
+        let novaData = dateFormatter.string(from: dataRecuperada as! Date)
+        
+        cell.textLabel?.text = descricaoRecuperado as? String
+        cell.detailTextLabel?.text = String(describing: novaData)
+        
+        return cell
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        self.tableView.deselectRow(at: indexPath, animated: true)
+        let movimentacao = self.movimentacoes[indexPath.row]
+        self.performSegue(withIdentifier: "verMovimentacao", sender: movimentacao)
+        
+    }
+
+   
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == .delete {
+            
+            let movimentacao = self.movimentacoes[indexPath.row]
+            
+            self.context.delete(movimentacao)
+            
+            self.movimentacoes.remove(at: indexPath.row)
+            
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            
+            do {
+                
+                try self.context.save()
+                
+                print("Movimentacao Removida Com Sucesso")
+                
+            } catch let erro {
+                print("Erro ao Remover Movimentacao : \(erro.localizedDescription) ")
+            }
+            
+        }
+    }
+
+
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+        
+        if segue.identifier == "verMovimentacao"{
+            let viewDestino = segue.destination as! ViewController
+            viewDestino.movimentacao = sender as? NSManagedObject
+        }
+        
+    }
+    
+
+}
---
+++ Movimentation/ViewController.swift
@@ -0,0 +1,105 @@
+//
+//  ViewController.swift
+//  Movimentation
+//
+//  Created by Gustavo Silva on 12/14/20.
+//  Copyright © 2020 home. All rights reserved.
+//
+//https://www.ioscreator.com/tutorials/display-date-date-picker-ios-tutorial
+
+import UIKit
+import CoreData
+
+class ViewController: UIViewController {
+    
+    let tipos = ["Debito","Credito"]
+    
+    @IBOutlet weak var descricao: UITextView!
+    @IBOutlet weak var tipo: UITextView!
+    @IBOutlet weak var data: UITextView!    
+    @IBOutlet weak var valor: UITextView!
+    
+    var context: NSManagedObjectContext!
+    var movimentacao: NSManagedObject!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        self.descricao.becomeFirstResponder()
+        
+        
+        if movimentacao != nil{
+            if let descricaoRecuperada = movimentacao.value(forKey: "descricao"){
+                self.descricao.text = String(describing: descricaoRecuperada)
+            }
+            if let valorRecuperado = movimentacao.value(forKey: "valor"){
+                self.valor.text = String(describing: valorRecuperado)
+            }
+            if let tipoRecuperado = movimentacao.value(forKey: "tipo"){
+                self.tipo.text = String(describing: tipoRecuperado)
+            }
+            if let dataRecuperada = movimentacao.value(forKey: "data"){
+                self.data.text = String(describing: dataRecuperada)
+            }
+        }else{
+            self.descricao.text = ""
+            self.valor.text = ""
+            self.tipo.text = ""
+            self.data.text = ""
+        }
+               
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        context = appDelegate.persistentContainer.viewContext
+        
+    }
+    
+    
+    @IBAction func salvarMovimentacao(_ sender: Any) {
+           
+        if movimentacao == nil{
+            self.salvar()
+        }else{
+            self.atualizar()
+        }
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    func salvar(){
+        
+        let movimentation = NSEntityDescription.insertNewObject(forEntityName: "Movement", into: context)
+        
+        movimentation.setValue(self.descricao.text, forKey: "descricao")
+        movimentation.setValue(self.tipo.text, forKey: "data")
+        movimentation.setValue(self.tipo.text, forKey: "tipo")
+        movimentation.setValue(self.valor.text, forKey: "valor")
+        
+        do {
+            try context.save()
+            print("Movimentacao Salva com Sucesso !!!!")
+        } catch let erro {
+            print("Erro ao Salvar a Movimentacao: \(erro.localizedDescription)")
+        }
+        
+    }
+    
+    func atualizar(){
+        
+        movimentacao.setValue(self.descricao.text, forKey: "descricao")
+        movimentacao.setValue(self.tipo.text, forKey: "data")
+        movimentacao.setValue(self.tipo.text, forKey: "tipo")
+        movimentacao.setValue(self.valor.text, forKey: "valor")
+                
+        do {
+            try context.save()
+            print("Movimentacao Atualizada com Sucesso !!!!")
+        } catch let erro {
+            print("Erro ao Atualizar a Movimentacao: \(erro.localizedDescription)")
+        }
+        
+    }
+    
+
+}
+
